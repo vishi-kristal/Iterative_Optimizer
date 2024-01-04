0e283a8d41af59116bed367b5d0553c959ff5e01
vishi-kristal <152411291+vishi-kristal@users.noreply.github.com>
2024-01-04 15:11:05 +0800

    Update main.py
    Updated email field to key in main.py

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ users = [
 },
 {
     'date_joined': '2024-01-04 00:00:00.000000',
-    'email': 'vishi.rajvanshi@kristal.ai',
+    'key': 'vishi.rajvanshi@kristal.ai',
     'password': '$2b$12$fGFPf.5EjCbHQ8HxbsOO5.U4vb0EH.PUD0xl6GcxK62KpTzRVVF8q',
     'username': 'vishi.rajvanshi@kristal.ai'}
 ]
